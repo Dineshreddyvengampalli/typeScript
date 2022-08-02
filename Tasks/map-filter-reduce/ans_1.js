20c92d4a6d33d018afe4076c313b750a29e6894d
Dineshreddyvengampalli <dineshreddyvengampalli@gmail.com>
2022-08-02 13:48:52 +0530

    modified 1 st answer

--- Tasks/map-filter-reduce/ans_1.js
+++ Tasks/map-filter-reduce/ans_1.js
@@ -2,6 +2,7 @@
 
 
 const { count, Console } = require('console');
+const { json } = require('stream/consumers');
 var jsonData = require('./sample.json');
 
 // let emails = jsonData.map((user,index)=>{
@@ -13,43 +14,58 @@ var jsonData = require('./sample.json');
 // console.log(emails)
 
 
-var jsonData = require('./sample.json');
-let reqData = []
-for(let i=0;i<jsonData.length;i++)
-{
-    for(let j=i+1;j<jsonData.length;j++)
-    {
-        if(jsonData[i].email==jsonData[j].email)
-        {
-            if(jsonData[i].updatedAt>jsonData[j].updatedAt)
-            {
-                reqData.push(jsonData[i])
-                i++
-                j++
+// var jsonData = require('./sample.json');
+// let reqData = []
+// for(let i=0;i<jsonData.length;i++)
+// {
+//     for(let j=i+1;j<jsonData.length;j++)
+//     {
+//         if(jsonData[i].email==jsonData[j].email)
+//         {
+//             if(jsonData[i].updatedAt>jsonData[j].updatedAt)
+//             {
+//                 reqData.push(jsonData[i])
+//                 i++
+//                 j++
                 
                 
-            }
-            else
-            {
-                reqData.push(jsonData[j])
-                j++
-                i++
+//             }
+//             else
+//             {
+//                 reqData.push(jsonData[j])
+//                 j++
+//                 i++
                 
-            }
+//             }
            
-        }
-        // else{
-        //     reqData.push(jsonData[i])
-        //     i++
+//         }
+//         // else{
+//         //     reqData.push(jsonData[i])
+//         //     i++
             
-        // }
+//         // }
         
         
 
+//     }
+// }
+
+// console.log(reqData)
+// console.log(jsonData.length)
+// console.log(reqData.length)
+
+
+let emails = jsonData.map((value,index)=>{
+    return value.email
+})
+
+let repeatedEmails = jsonData.filter((value,index,self)=>{
+    return (self.indexOf(value) !== index)
+})
+
+let uniqueUsers = jsonData.map((value,index)=>{
+    if((value.email==repeatedEmails)){
     }
-}
+})
 
-console.log(reqData)
-console.log(jsonData.length)
-console.log(reqData.length)
-
+console.log(repeatedEmails)
--- Tasks/map-filter-reduce/ans_4.js
+++ Tasks/map-filter-reduce/ans_4.js
@@ -1,100 +1,57 @@
 // 4.Get All users's ids who are using same 'apiKey'
 
+const { some } = require('async');
 const { count, Console } = require('console');
 var jsonData = require('./sample.json');
 
-// let getId = ()=>{
-//     let sampleData = []
-//     for (let i=0;i<jsonData.length;i++){
-//         for(let j=i+1;j<jsonData.length;j++){
-//             if(jsonData[i].apiKey!=jsonData[j].apiKey){
-//                 sampleData.push[jsonData[i]]
-//                 i = i+1
-//                 j = j+1
-//             }
-//         }
 
-//     }
-//     return sampleData
-// }
-// let data = getId()
-// console.log(data)
-// let getId = ()=>{
-//     let sampleData = []
-//     for(let i = 0;i<jsonData.length;i++){
-//         for(let j=i+1;j<jsonData.length;j++){
-//             if(jsonData[i].apiKey==jsonData[j].apiKey){
-//                 sampleData.push(jsonData[i].id, jsonData[j].id)
-//                 i++
-//                 j++
+let keys = jsonData.map((value,index)=>{
+    return value.apiKey
+})
 
-//             }
+let repeatedKeys = keys.filter((value,index,self)=>{
+    return self.indexOf(value) !== index
+})
+
+let repeatedKeysFilter = repeatedKeys.filter((value,index,self)=>{
+    return self.indexOf(value) === index
+})
+
+// let usersWithSameIds = jsonData.filter((value,index)=>{
+//     for(let i of repeatedKeysFilter){
+//         if(i==value.apiKey){
+//             return value.apiKey
 //         }
 //     }
-//     return sampleData
-// }
-
-// let data = getId()
-// console.log(data)
-
-// userIds = jsonData.map((data)=>{
+// })
+// let idsWithSameKeys = usersWithSameIds.map((data)=>{
 //     return data.id
 // })
-// let keys = jsonData.map((data)=>{
-//     return data.apiKey
-// })
-// reqUserIds = []
-// for(let i=0;i<keys.length;i++){
-//     for(let j=i+1;j<keys.length;j++){
-//         if(keys[i]==keys[j]){
-//             // reqUserIds.push[(userIds[i],userIds[j])]
-//             // console.log(userIds[i])
-//             // reqUserIds[userIds[i]] = [(userIds[j])]
-//             reqUserIds.push(userIds[i],userIds[j])
 
-//         }
-//     }
-// }
-// // console.log(userIds)
 
-// // console.log(keys)
-
-// console.log(reqUserIds)
-
-// let reqUserIds = jsonData.filter((data)=>{
-//     let allUserids = []
-//     allUserids.push(data.id)
-//     let allApiKeys = []
-//     allApiKeys.push(data.apiKey)
-//     return 
-// })
-
-let keys = jsonData.map((data)=>{
-    return data.apiKey
-})
-// console.log(keys)
-
-let multipleKey = keys.filter((value, index, self) => {
-    // console.log(self)
-    return (self.indexOf(value) !== index)
- 
+let usersWithSameIds = repeatedKeys.map((value,index)=>{
+    let fake = jsonData.filter((data)=>{
+        return data.apiKey === value
+    })
+    return fake
 })
 
-// console.log(keys.length)
-/// console.log(jsonData.apiKey in multipleKey)
-console.log(multipleKey)
-let lis =[]
-for(let i=0;i<jsonData.length;i++){
-    for(let j=0;j<multipleKey.length;j++){
-        if(multipleKey[j]==jsonData[i].apiKey){
-            lis.push(jsonData[i].id)
-            break
-        }
-    }
-}
-console.log(lis)
-// reqData = jsonData.filter((value, index, self) => {
-//     console.log(self)
-//     return self.indexOf(value) === index
-//   })
 
+let reduceUsers = usersWithSameIds.reduce((a,b)=>{
+   return a.concat(b)
+     
+},[])
+
+let idsWithSameKeys = reduceUsers.map((data)=>{
+    return data.id
+    
+})
+
+let finalAns = idsWithSameKeys.filter((value,index,self)=>{
+    return self.indexOf(value) === index
+})
+
+// console.log(repeatedKeysFilter)
+// console.log(usersWithSameIds)
+
+console.log(finalAns)
