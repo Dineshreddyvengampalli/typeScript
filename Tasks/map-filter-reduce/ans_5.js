edb652c1e414b580a4ddfcff260864ed04df256e
Dineshreddyvengampalli <dineshreddyvengampalli@gmail.com>
2022-08-08 21:56:24 +0530

    modified ans

--- Tasks/map-filter-reduce/ans_5.js
+++ Tasks/map-filter-reduce/ans_5.js
@@ -17,26 +17,25 @@ let data = require('./sample.json')
 // Krishna Janmastami (19th August)
 
 let dobIn1989 = data.filter((value)=>{
-    value['dob'] = moment(value.profile.dob)
-    return dateTime.year() == 1989
+    return moment(value.profile.dob).year() == 1989
 
 })
 
 let dobIn1992 = data.filter((value)=>{
-    value['dob'] = moment(value.profile.dob)
-    return value.dob.year() == 1992
+    return (moment(value.profile.dob).year() == 1992)
 
 })
 
 let dobInJan = data.filter((value,index)=>{
-    return value.dob.month() == 00
+    return (moment(value.profile.dob).month() == 00)
 })
 
 let dobInDec = data.filter((value,index)=>{
-    return value.dob.month() == 11
+    return (moment(value.profile.dob).month() == 11)
 })
 
 let krishnaBirth = data.filter((value,index)=>{
-    return value.dob.month() == 07 && value.dob.date() == 19
+    return (moment(value.profile.dob).date() == 19 && moment(value.profile.dob).month()==07)
 })
 
+console.log(krishnaBirth)
