2287219c3e95c2fe34c7874db8bc922bf9430c92
Dineshreddyvengampalli <dineshreddyvengampalli@gmail.com>
2022-08-12 23:18:13 +0530

    added testing for aws-sdk

--- 21.aws/secretManager/secretKeyGetter.test.js
+++ 21.aws/secretManager/secretKeyGetter.test.js
@@ -1,13 +1,36 @@
-let test = require('jest')
-let secretGetter  = require('./secretKeyGetter')
-test('testing secret key getter',()=>{
-    let secretKeyMock = jest.fn(secretGetter())
-    let SecretsManager = jest.fn()
-    SecretsManager.mockImplementation(()=>{
-
-    })
-    secretKeyMock.mockImplementation(()=>{
-        
-
-    })
+let getToken = require('./secretKeyGetter')
+let AWS = require('aws-sdk')
+// let jest = require('jest')
+let mockSecretValue = jest.fn((secretId)=>{
+    return (`this is mocked value for ${secretId}`)
 })
+
+jest.mock('aws-sdk',()=>{
+    return{
+        SecretsManager : jest.fn(()=>{
+            return{
+                getSecretValue : jest.fn(({secretId})=>{
+                    mockSecretValue(secretId)
+                })
+            }
+        })
+    }
+})
+
+let res = getToken("sampleApp/prod/config")
+
+test("test for mocked function",()=>{
+    let spy = jest.spyOn(AWS,'SecretsManager')
+    expect(spy).toHaveBeenCalledTimes(1)
+
+})
+
+test("testing of mock secret value",()=>{
+    expect(mockSecretValue).toHaveBeenCalledTimes(1)
+
+})
+
+test("testing of response type",()=>{
+    expect(res).toBeUndefined
+})
+
